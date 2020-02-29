bf258132768fe72b3b3464fc3e5c51f0a3223d9d
Kleiton Marques <kleitonm.marques@gmail.com>
2020-02-29 20:28:44 -0300

    Componentes Filhos

---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menber from './menber'
+
+export default props => (
+    <div>
+        <Menber name="João" lastName="Silva" />
+        <Menber name="Maria" lastName="Silva" />
+        <Menber name="Jose" lastName="Silva" />
+        <Menber name="Pedro" lastName="Silva" />
+    </div>
+)
